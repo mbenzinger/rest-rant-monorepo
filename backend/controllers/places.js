2ff9f19bff3f1356a9117aec3436128675b71720
Mason Benzinger <mason.benzinger@gmail.com>
2022-10-29 12:10:28 -0500

    added day 5 in-class activity up through step 3, stopped working

--- backend/controllers/places.js
+++ backend/controllers/places.js
@@ -94,16 +94,39 @@ router.post('/:placeId/comments', async (req, res) => {
         res.status(404).json({ message: `Could not find place with id "${placeId}"` })
     }
 
-    const author = await User.findOne({
-        where: { userId: req.body.authorId }
-    })
+    let currentUser;
+    try {
+        const [method, token] = req.headers.authorization.split(' ')
+        if (method == 'Bearer') {
+            const result = await jwt.decode(process.env.JWT_SECRET, token)
+            const { id } = result.value
+            currentUser = await User.findOne({
+                where: {
+                    userId: id
+                }
+            })
+        }
+    } catch {
+        currentUser = null
+    }
 
-    if (!author) {
-        res.status(404).json({ message: `Could not find author with id "${req.body.authorId}"` })
+    // const author = await User.findOne({
+    //     where: { userId: req.body.authorId }
+    // })
+
+    // if (!author) {
+    //     res.status(404).json({ message: `Could not find author with id "${req.body.authorId}"` })
+    // }
+
+    if (!currentUser) {
+        return res.status(404).json({
+            message: `You must be logged in to leave a rant or rave.`
+        })
     }
 
     const comment = await Comment.create({
         ...req.body,
+        authorId: currentUser.userId,
         placeId: placeId
     })
 
